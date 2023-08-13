e0b7b01a2a29137d54593f03b5bcaf6be49f65af
Anh Bui <anhbhpd08263@fpt.edu.vn>
2023-08-13 23:39:14 +0700

    update project FashionShop-PHP final

--- admin/modules/dashboard.php
+++ admin/modules/dashboard.php
@@ -16,8 +16,26 @@
             <div class="card mb-4">
                 <div class="card-body">
                     <h5 class="card-title">Category Management</h5>
-                    <p class="card-text">Quản lý danh mục sản phẩm của bạn, tạo danh mục mới hoặc xóa danh mục.</p>
+                    <p class="card-text">Quản lý danh mục sản phẩm của bạn, tạo danh mục mới, cập nhật hoặc xóa danh mục.</p>
                     <a href="index.php?action=quanlydanhmuc&query=add" class="btn btn-primary">Go to Category Management</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card mb-4">
+                <div class="card-body">
+                    <h5 class="card-title">Orders Management</h5>
+                    <p class="card-text">Quản lý đơn đặt hàng sản phẩm của khách hàng, xem thông tin chi tiết đơn hàng.</p>
+                    <a href="index.php?action=quanlydonhang&query=lietke" class="btn btn-primary">Go to Orders Management</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card mb-4">
+                <div class="card-body">
+                    <h5 class="card-title">Customers Management</h5>
+                    <p class="card-text">Quản lý thông tin của khách hàng, có thể xoá đi khách hàng không mong muốn.</p>
+                    <a href="index.php?action=quanlykhachhang&query=lietke" class="btn btn-primary">Go to Customers Management</a>
                 </div>
             </div>
         </div>
